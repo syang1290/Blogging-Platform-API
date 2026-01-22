a6dd0e5a3bface3f80b2a2a3a36746669125125a
syang1290 <stevey13@uci.edu>
2026-01-22 14:51:36 -0800

    Added CORS

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sqlite3
 
 app = Flask(__name__)
+CORS(app)
 
 def get_db_connection():
     conn = sqlite3.connect('blog.db')
